4cf882220fdc7fc27a501ca441f3c406d45066ee
howfishy <58379130+howfishy@users.noreply.github.com>
2025-03-16 19:08:49 +0800

    Initial commit for my cozy bookstore website

---
+++ script.js
@@ -0,0 +1,41 @@
+// Add click interactions
+document.getElementById('pearl').addEventListener('click', function() {
+    alert('Pearl floats around playfully!');
+});
+
+document.getElementById('moody').addEventListener('click', function() {
+    alert('Moody glares at you grumpily.');
+});
+
+document.getElementById('bookshelf').addEventListener('click', function() {
+    alert('This will lead to the Reading Tracker (Coming Soon)');
+});
+
+document.getElementById('noticeboard').addEventListener('click', function() {
+    alert('This will open the About Page (Coming Soon)');
+});
+
+document.getElementById('coffee').addEventListener('click', function() {
+    alert('Mmm... smells nice!');
+});
+
+document.getElementById('window').addEventListener('click', function() {
+    alert('A beautiful view of the outside.');
+});
+
+// Sidebar Buttons (Just placeholders for now)
+document.getElementById('btn-time').addEventListener('click', function() {
+    alert('Time change feature coming soon!');
+});
+
+document.getElementById('btn-weather').addEventListener('click', function() {
+    alert('Weather change feature coming soon!');
+});
+
+document.getElementById('btn-books').addEventListener('click', function() {
+    alert('Book Collection feature coming soon!');
+});
+
+document.getElementById('btn-about').addEventListener('click', function() {
+    alert('About Page coming soon!');
+});
